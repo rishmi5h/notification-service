135106206746cd0f37649232869167556e616ffd
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-07-05 12:44:52 +0530

    add(Mailing Service): to send mails whe order placed

---
+++ src/main/java/com/rishmi5h/notification/service/NotificationService.java
@@ -0,0 +1,45 @@
+package com.rishmi5h.notification.service;
+
+import com.rishmi5h.notification.order.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed", groupId = "notification")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got Message form order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            mimeMessage.setFrom("rishabhmishrabu@gmail.com");
+            mimeMessage.setText(orderPlacedEvent.getEmail());
+            mimeMessage.setSubject(String.format("Your Order with id %s is placed successfully", orderPlacedEvent.getOrderId()));
+            mimeMessage.setText(String.format("""
+                    Dear Customer,
+                                       \s
+                    Your order with id %s is placed successfully.
+                    Thanks for shopping with us.
+                                       \s
+                    Regards,
+                    Black Unlimited Team
+                   \s""", orderPlacedEvent.getOrderId()));
+        };
+
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notification email sent successfully");
+        } catch (MailException e) {
+            log.error("Error while sending mail", e);
+            throw new RuntimeException("Exception while sending mail to rishabhmishrabu@gmail.com", e);
+        }
+    }
+}
